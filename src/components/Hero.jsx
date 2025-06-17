0f0f92a2ad5f19ef55ee5a7c5736e1d2affe84c3
sivachelvan2002 <sivachelvan2002@gmail.com>
2025-06-17 23:02:19 +0530

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 export default function () {
   return (
-    <div>
-        <div className='w-full h-screen bg-[#2c2e2b]'></div>
+    <div className='border border-amber-300  w-full min-h-screen bg-[#2c2e2b]'>
+        <div className='mycontainer min-h-screen w-full mx-auto md:max-w-6xl  text-gray-100 flex flex-col items-center justify-center gap-y-5'>
+          <h1 className=' text-center text-3xl md:text-5xl xl:text-6xl font-semibold tracking-wider'>Build Stunning Websites Smarter And Faster with AI-Automation</h1>
+           <p className='text-center text-xl md:text-2xl max-w-3xl'>Use AI to create polished, responsive websites in a matter of minutes without any code required.</p>
+           <button className='py-3 px-6 bg-green-500 hover:bg-green-600 hover:cursor-pointer transition-all duration-300 border-0 rounded text-lg font-semibold '>Build websites now for free</button>
+
+        </div>
     </div>
   )
 }
+
+
+
+{/* <div className='h-full w-full bg-[#2c2e2b] flex flex-col items-center justify-center '>
+          <h1 className=' text-center text-3xl font-black text-[#ffffff] pt-15 md:text-6xl'>Build Stunning Websites Smarter And Faster with AI-Automation</h1>
+          <p className='text-centertext-3xl text-[#ffffff]'>Use AI to create polished, responsive websites in a matter of minutes without any code required.</p>
+          <button className='text-xl font-bold text-[#ffffff] w-96 h-12 bg-green-500 p-4 flex flex-col justify-center items-center cursor-pointer rounded-2xl hover:bg-green-900 transition-all shadow shadow-green-700 duration-400'>Build websites now for free</button>
+        </div> */}
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,8 +8,8 @@ export default function Navigation() {
     const [menuStatus,setMenuStatus]=useState(false)
    
   return (
-    <div>
-        <nav className='mycontainer w-full h-24 bg-[#0a0a0a] border-b border-green-500 shadow  flex flex-row justify-between items-center text-sm md:text-md '>
+    <div className='relative'>
+        <nav className='mycontainer w-full h-24 bg-[#0a0a0a] border-b border-green-500 shadow  flex flex-row justify-between items-center text-sm md:text-md fixed z-50'>
             <img src="./logo1.png" alt="logo" className='w-38 h-12' />
             <div className=' flex-row  items-center  md:gap-4 lg:gap-12  hidden md:flex'>
             <a href="" className='nav-link '>Features</a>
@@ -26,16 +26,20 @@ export default function Navigation() {
             
            </div>
         </nav>
-        {menuStatus&& ( <div className='md:hidden bg-[#0a0a0a] px-6 py-4 space-y-4 text-white flex  flex-col justify-center items-center font-bold '>
-                    <a href="#" className="block nav-link">Features</a>
-                    <a href="#" className="block nav-link">How it Works</a>
-                    <a href="#" className="block nav-link">Integration</a>
-                    <a href="#" className="block nav-link">Tech Stack</a>
-                    <a href="#" className="block nav-link">Pricing</a>
-                    <button className='w-full mt-2 py-2 rounded-full text-white font-semibold nav-button:hover nav-button'>Get Started</button>
-                
-        </div>
-          )}
+        {menuStatus && (
+  <div
+    className={`md:hidden bg-[#0a0a0a] px-6 py-4 space-y-4 text-white flex flex-col justify-center items-center font-bold absolute top-0 w-full min-h-[80vh] z-30
+      transition-left duration-500 ease-in-out 
+      ${menuStatus ? "left-0" : "left-[100%]"}`}
+  >
+    <a href="#" className="block nav-link">Features</a>
+    <a href="#" className="block nav-link">How it Works</a>
+    <a href="#" className="block nav-link">Integration</a>
+    <a href="#" className="block nav-link">Tech Stack</a>
+    <a href="#" className="block nav-link">Pricing</a>
+    <button className='w-full mt-2 py-2 rounded-full text-white font-semibold nav-button:hover nav-button'>Get Started</button>
+  </div>
+)}
 
        
     </div>
